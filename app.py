5a818eee8cf89c085e9dc9b3cd6da85bb3488e67
pixelnoob <chitty@eosargentina.io>
2025-03-28 13:06:04 +0100

    limit register

--- app.py
+++ app.py
@@ -139,6 +139,7 @@ def test():
     return "This is a test endpoint."
 
 @app.route("/register", methods=["GET", "POST"])
+@limiter.limit("3/hour", override_defaults=False)
 def register():
     form = RegisterForm()
     if form.validate_on_submit():
